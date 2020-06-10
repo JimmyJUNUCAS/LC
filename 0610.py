d06a5fc8f2021fa77655a5d0b898e9d6b141adca
Jimmy <467120567@qq.com>
2020-06-11 00:24:08 +0800

    Create 0610.py

---
+++ 0610.py
@@ -0,0 +1,97 @@
+class TreeNode(object):
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+l1_1 = TreeNode(1)
+l1_2 = TreeNode(2)
+l1_3 = TreeNode(3)
+l1_4 = TreeNode(4)
+l1_5 = TreeNode(5)
+l1_6 = TreeNode(6)
+l1_7 = TreeNode(7)
+
+l1_1.left = l1_2
+l1_1.right = l1_7
+l1_2.left = l1_3
+l1_2.right = l1_4
+l1_4.left = l1_5
+l1_4.right = l1_6
+
+def inorderTraversal(root):
+    res = []
+    def dfs(root):
+        if not root : return 0
+        dfs(root.left)
+        res.append(root.val)
+        dfs(root.right)
+    dfs(root)
+    return res
+print(inorderTraversal(l1_1))
+
+def preorderTraversal(root):
+    res = []
+    def dfs(root):
+        if not root : return 0
+        res.append(root.val)
+        dfs(root.left)
+        dfs(root.right)
+    dfs(root)
+    return res
+print(preorderTraversal(l1_1))
+
+def postorderTraversal(root):
+    res = []
+    def dfs(root):
+        if not root : return 0
+        dfs(root.left)
+        dfs(root.right)
+        res.append(root.val)
+    dfs(root)
+    return res
+print(postorderTraversal(l1_1))
+
+def inorderTraversal(root):
+    stack = []
+    res = []
+    while root or stack:
+        if root :
+            stack.append(root)
+            root = root.left
+        else:
+            tmp = stack.pop()
+            res.append(tmp.val)
+            root = tmp.right
+    return res
+print(inorderTraversal(l1_1))
+
+def preorderTraversal(root):
+    stack = [root,]
+    res = []
+    if root is None : return 0
+    while stack:
+        root = stack.pop()
+        if root is not None:
+            res.append(root.val)
+            if root.right is not None:
+                stack.append(root.right)
+            if root.left is not None:
+                stack.append(root.left)
+    return res
+print(preorderTraversal(l1_1))
+
+def postorderTraversal(root):
+    stack = [root,]
+    res = []
+    if root is None : return 0
+    while stack:
+        root = stack.pop()
+        if root is not None:
+            res.append(root.val)
+            if root.left is not None:
+                stack.append(root.left)
+            if root.right is not None:
+                stack.append(root.right)
+    return res[::-1]
+print(postorderTraversal(l1_1))
+
